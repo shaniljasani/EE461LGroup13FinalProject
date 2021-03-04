95b3fb8e9fdadf26d6c71c83309373bcf55c219e
Shanil Jasani <shaniljasani@utexas.edu>
2021-03-03 18:05:16 -0600

    Add dotenv for Secret Key

--- AutoCar/autocar.py
+++ AutoCar/autocar.py
@@ -4,11 +4,16 @@
 # Notes
 # Trailing slash fix https://stackoverflow.com/questions/40365390/trailing-slash-in-flask-route
 
+import os
 
+from dotenv import load_dotenv
 from flask import Flask, render_template
 
+# import config env file
+load_dotenv(dotenv_path="./.env")
 
 app = Flask(__name__, static_url_path="/static")
+app.secret_key = os.getenv("APP_SECRET") # secret key used for cookies in the future
 
 @app.route('/')
 def index():
