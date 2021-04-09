9b9fea2ea0cc3a1599b95a4b71c49047f98d3cb0
JulianFritz <julianfritz@utexas.edu>
2021-04-09 17:57:20 -0500

    Added some database comments

--- AutoCar/database/cardb.py
+++ AutoCar/database/cardb.py
@@ -1,7 +1,7 @@
 # need: function that formats into a post properly
 # make, model, year, status
 
-
+# create a new car post for the db and define the fields that it has
 def new_car_post(carID, mk, md, yr, descrip):
     post = {"carID": carID,
             "make": mk,
@@ -11,7 +11,7 @@ def new_car_post(carID, mk, md, yr, descrip):
             "checked_out": False}
     return post
 
-
+# change the checkout status of a car
 def change_car_status(carID, status):
     post = ({"carID": carID},
             {
@@ -21,7 +21,7 @@ def change_car_status(carID, status):
             })
     return post
 
-
+# change the description for that car
 def update_car_description(carID, descrip):
     post = ({"carID": carID},
             {
--- AutoCar/database/userdb.py
+++ AutoCar/database/userdb.py
@@ -1,14 +1,14 @@
 # need: function that formats into a post properly
 # payment info, personal details, password, username, history (carshareid list)
 
-
+# create a new user for the db and define the fields that it has
 def new_user_post(usernm, password):
     post = {"username": usernm,
             "password": password,
             "history": []}
     return post
 
-
+# add something to that user's history
 def add_to_user_history(usernm, newHistory):
     post = ({"username": usernm},
             {
