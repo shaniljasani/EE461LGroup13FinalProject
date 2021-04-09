8c393abb814d71678f020c4c2220339f68ca53dd
JulianFritz <julianfritz@utexas.edu>
2021-04-09 17:40:16 -0500

    Added backend comments

--- AutoCar/autocar.py
+++ AutoCar/autocar.py
@@ -17,53 +17,58 @@ import pandas as pd
 
 app = Flask(__name__, static_url_path="/static")
 app.secret_key = os.getenv("APP_SECRET") # secret key used for cookies in the future
-app.secret_key = 'secret key'
-app.register_blueprint(auth_bp)
+app.secret_key = 'secret key' # for now use just 'secret key'
+app.register_blueprint(auth_bp) 
 app.register_blueprint(car_bp)
 
+# the home/main page
 @app.route('/')
 def index():
     return render_template("index.html")
 
-
+# the dashboard page that shows cars available and carshares
 @app.route('/dashboard')
 def dashboard():
     db = ManageDB()
+    # if the user is logged in, show their carshares as well as available cars
     if(session.get('username')):
+        # get the carshares and available cars from the database and display them in the dashboard template
         return render_template("dashboard.html", carshares=db.find_user(session.get('username')).get('history'), available_cars = db.get_all_available_cars())
+    # else if no one is logged in just show the available cars
     return render_template("dashboard.html", available_cars = db.get_all_available_cars())
+    # close the db so we don't make copies
     db.close()
 
+# the dataset downloads page
 @app.route('/downloads')
 def file_downloads():
-    try:
-        return render_template('downloads.html')
-    except Exception as e:
-        return e
+    return render_template('downloads.html')
 
+# the route to send car makes download to the user
 @app.route('/return-carmakes')
 def return_files():
-    db = ManageDB()
-    #TODO figure out how to get car data from Mongo as a csv that can be sent using send_file
+    # downloads a file to the user that contains a dataset of all car makes.
+    return redirect('https://vpic.nhtsa.dot.gov/api/vehicles/GetAllMakes?format=csv')
+    # TODO
+    # find a different way to get the download to work, using our website's data instead of nhtsa?
+
+
+    #db = ManageDB()
     #collection = db.get_cars_collection()
     #cursor = collection.find() # returns every item in the collection
     #mongoItems = list(cursor)
     #df = pd.DataFrame(mongoItems)
     #carcsv = df.to_csv(sep=',')
-
     #try:
         #return send_file(carcsv)#, as_attachment=True, attachment_filename='cars.csv')
     #except Exception as e:
         #return e
+    #db.close()
 
-    # this gives the user a download file that contains a dataset of all car Makes.
-    # Not 
-    return redirect('https://vpic.nhtsa.dot.gov/api/vehicles/GetAllMakes?format=csv')
-
-    db.close()
-
+# the route to send tesla models download to the user
 @app.route('/return-teslamodels')
 def redirect_tesla_download():
+    # downloads a file to the user that contains tesla models
     return redirect('https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMake/tesla?format=csv')
 
 if __name__ == "__main__":
