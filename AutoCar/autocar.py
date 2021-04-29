a1e27c3ce6ede42b3e821633e9569f26e848d631
JulianFritz <julianfritz@utexas.edu>
2021-04-29 18:57:30 -0500

    Updated downloads page, added downloads.py

--- AutoCar/autocar.py
+++ AutoCar/autocar.py
@@ -9,6 +9,7 @@ import os
 from flask import Flask, render_template, g, session, send_file, redirect
 from auth.auth import auth_bp
 from reserve.car import car_bp
+from downloads.downloads import downloads_bp
 from database.managedb import ManageDB 
 #TODO add pandas to requirements for installing
 import pandas as pd
@@ -20,6 +21,7 @@ app.secret_key = os.getenv("APP_SECRET") # secret key used for cookies in the fu
 app.secret_key = 'secret key' # for now use just 'secret key'
 app.register_blueprint(auth_bp) 
 app.register_blueprint(car_bp)
+app.register_blueprint(downloads_bp)
 
 # redirect on trainling slashes
 @app.before_request
@@ -60,32 +62,6 @@ def billing():
 def file_downloads():
     return render_template('downloads.html')
 
-# the route to send car makes download to the user
-@app.route('/return-carmakes')
-def return_files():
-    # downloads a file to the user that contains a dataset of all car makes.
-    return redirect('https://vpic.nhtsa.dot.gov/api/vehicles/GetAllMakes?format=csv')
-    # TODO
-    # find a different way to get the download to work, using our website's data instead of nhtsa?
-
-
-    #db = ManageDB()
-    #collection = db.get_cars_collection()
-    #cursor = collection.find() # returns every item in the collection
-    #mongoItems = list(cursor)
-    #df = pd.DataFrame(mongoItems)
-    #carcsv = df.to_csv(sep=',')
-    #try:
-        #return send_file(carcsv)#, as_attachment=True, attachment_filename='cars.csv')
-    #except Exception as e:
-        #return e
-    #db.close()
-
-# the route to send tesla models download to the user
-@app.route('/return-teslamodels')
-def redirect_tesla_download():
-    # downloads a file to the user that contains tesla models
-    return redirect('https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMake/tesla?format=csv')
 
 if __name__ == "__main__":
     app.run(debug=True, host="localhost")
